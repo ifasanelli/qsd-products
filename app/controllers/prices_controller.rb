8f963dd70dd46dbfa43f796081f7cab30a8fd0f5
Johny <johnycaribe@gmail.com>
2020-02-07 21:01:01 -0300

    register prices

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,4 +1,27 @@
 class PricesController < ApplicationController
+  before_action :load_plans_and_periodicities, only: %i[new create]
+
   def index
+  end
+
+  def new
+    @price = Price.new
+  end
+
+  def create
+    @price = Price.new(price_params)
+    @price.save
+    redirect_to root_path, notice: t('.success')
+  end
+
+  private
+
+  def price_params
+    params.require(:price).permit(:plan_price, :plan_id, :periodicity_id)
+  end
+
+  def load_plans_and_periodicities
+    @plans = Plan.all
+    @periodicities = Periodicity.all
   end
 end
--- spec/features/price/user_register_price_spec.rb
+++ spec/features/price/user_register_price_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 feature 'user register a new price' do
-  xscenario 'succesfully' do
+  scenario 'succesfully' do
     create(:plan, name: 'Hospedagem Básica')
     create(:periodicity, name: 'Mensal')
 
     visit root_path
     click_on 'Preços'
-    click_on 'Cadastrar Preço'
-    select 'Hospedagem Básica', from: 'Plano'
-    select 'Mensal', from: 'Periodicidade'
     click_on 'Registrar novo Preço'
-    fill_in 'Preço do Plano', with: 132.72
     select 'Hospedagem Básica', from: 'Plano'
     select 'Mensal', from: 'Periodicidade'
+    fill_in 'Preço do Plano', with: 132.72
     click_on 'Criar Preço'
 
     expect(page).to have_content('Preço registrado com sucesso')
